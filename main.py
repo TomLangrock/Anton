277feefd16e53c75af9b3e8ebce4e3001d79b4f8
Tom Langrock <noneOfyour@business.com>
2021-04-28 20:00:16 +0200

    AI beginnings

---
+++ main.py
@@ -0,0 +1,91 @@
+import turtle as t
+from tmaths import line, ray
+from random import randint
+
+linesegments = []
+
+bob = t.Turtle()
+s = bob.getscreen()
+s.tracer(4000,0)
+s.bgcolor("black")
+
+width = 1000
+widthpanel = width//2
+height = 400
+
+s.setup(width,height)
+s.setworldcoordinates(0,0,width, height)
+
+xBox = 50
+yBox = 50
+
+minProgress = 20
+maxProgress = 50
+
+bob.hideturtle()
+
+x = randint(0, xBox)
+y = randint(0, yBox)
+start = (x,y)
+bob.penup()
+bob.goto(x,y)
+
+
+bob.fillcolor("white")
+bob.begin_fill()
+bob.begin_poly()
+while bob.pos()[0] < widthpanel - xBox:
+  xtemp = x
+  ytemp = y
+  x = randint(x + minProgress , x + maxProgress)
+  y = randint(0, yBox)
+  bob.goto(x,y)
+  linesegments.append(line(xtemp,ytemp,x,y))
+
+while bob.pos()[1] < height - yBox:
+  xtemp = x
+  ytemp = y
+  x = randint(widthpanel  - xBox, widthpanel)
+  y = randint(y + minProgress , y + maxProgress)
+  bob.goto(x,y)
+  linesegments.append(line(xtemp,ytemp,x,y))
+
+while bob.pos()[0] > xBox:
+  xtemp = x
+  ytemp = y
+  x = randint(x - maxProgress , x - minProgress)
+  y = randint(height - yBox ,height)
+  bob.goto(x,y)
+  linesegments.append(line(xtemp,ytemp,x,y))
+
+while bob.pos()[1] > 1.5*yBox:
+  xtemp = x
+  ytemp = y
+  x = randint(0, xBox)
+  y = randint(y - maxProgress , y - minProgress)
+  bob.goto(x,y)
+  linesegments.append(line(xtemp,ytemp,x,y))
+
+linesegments.append(line(x,y,*start))
+bob.goto(start)
+bob.end_poly()
+bob.end_fill()
+walls = bob.get_poly()
+
+
+p = (widthpanel//2,height//2)
+r = (1,0)
+first = ray(p, r, bob)
+
+
+s.clear()
+s.bgcolor("black")
+bob.goto(start)
+bob.fillcolor("white")
+bob.begin_fill()
+for point in walls:
+  bob.goto(point)
+bob.end_fill()
+for line in linesegments:
+  first.intersectsWithLinesegment(line)
+s.update()
---
+++ tmaths.py
@@ -0,0 +1,34 @@
+class line:
+    def __init__(self,xstart,ystart,xstop,ystop):
+        self.xstart = xstart
+        self.ystart = ystart
+        self.xstop = xstop
+        self.ystop = ystop
+    
+    def __repr__(self):
+        repr = "(("
+        repr += str(self.xstart) + ", " 
+        repr += str(self.ystart) + "), (" 
+        repr += str(self.xstop) + ", " 
+        repr += str(self.ystop) + "))"
+        return repr
+
+class ray:
+    def __init__(self, p, r , turtle):
+        self.p = p
+        self.r = r
+        self.turtle = turtle
+
+    
+    def intersectsWithLinesegment(self, linesegment):
+        num = self.r[1]*(linesegment.xstart - self.p[0])+self.r[0]*(self.p[1]-linesegment.ystart) 
+        denom = self.r[1]*(linesegment.xstart - linesegment.xstop)+self.r[0]*(linesegment.ystop-linesegment.ystart)
+        if denom != 0:
+            v = num/denom
+        else:
+            v = None
+
+        if 0 < v < 1:
+            self.turtle.goto(linesegment.xstart + v*(linesegment.xstop-linesegment.xstart), linesegment.ystart + v*(linesegment.ystop-linesegment.ystart) )
+            self.turtle.pencolor("red")
+            self.turtle.dot()
